789b5821e19fef650a362cb6aae88346ff086143
nupurgoyal09 <69473748+nupurgoyal09@users.noreply.github.com>
2020-12-22 11:06:17 +0530

    Add files via upload

---
+++ Paper.js
@@ -0,0 +1,28 @@
+class Paper{
+    constructor(x,y,r){
+        var options={
+            isStatic:false,
+            restitution:0.3,
+            friction:0.5,
+            density:1.2
+
+        }
+       this.x=x;
+       this.y=y;
+       this.r=r;
+       this.image=loadImage("paper.png");
+       this.body=Bodies.circle(this.x,this.y,this.r/2,options)
+       World.add(world,this.body); 
+    }
+    display(){
+        var paperpos=this.body.position;
+
+        push()
+        translate(paperpos.x,paperpos.y);
+        rotate(this.body.angle);
+        image(this.image,0,0,80,80);
+        pop()
+
+
+    }
+}
